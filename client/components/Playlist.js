d633d81ae6b2d33374823f172cdde340601e7a82
Eileen Galindo <galindo.eileen@gmail.com>
2019-05-07 18:03:38 -0400

    added continous playback functionality

--- client/components/Playlist.js
+++ client/components/Playlist.js
@@ -1,74 +1,75 @@
-import React, {Component} from 'react';
-import Player from './Player';
-import {connect} from 'react-redux';
-import {addSongThunk} from '../store/playlist';
+import React, {Component} from 'react'
+import Player from './Player'
+import {connect} from 'react-redux'
+import {addSongThunk} from '../store/playlist'
 import SearchForm from './SearchForm'
 
-const audio = document.createElement('audio');
+const audio = document.createElement('audio')
 
 class Playlist extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      artistSearch: '',
-      selectedSong: '',
-      playing: false
-    };
-    this.playSong = this.playSong.bind(this);
+      selectedSong: ''
+    }
+
+    this.nextTrack = this.nextTrack.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount() {
-    this.setState({selectedSong: this.props.playlist.currentSong.audioUrl});
+  async handleSubmit(event) {
+    try {
+      event.preventDefault()
+      await this.props.addSong(this.props.form.search.values.trackSearch)
+      this.setState({selectedSong: this.props.playlist.songList[0].audioUrl})
+    } catch (error) {
+      console.error(error.message)
+    }
   }
 
-  handleSubmit(event){
-    event.preventDefault()
-    this.props.addSong(this.props.form.search.values.trackSearch);
-  }
-
-  playSong() {
-
-    audio.pause();
-
-    audio.src = this.props.playlist.currentSong.audioUrl;
-    this.setState(prevState => ({playing: !prevState.playing}));
-    audio.load();
-    audio.play();
+  nextTrack() {
+    if (this.props.playlist.songList.length >= 1) {
+      this.props.playlist.songList.shift()
+      this.setState({selectedSong: this.props.playlist.songList[0].audioUrl})
+    }
   }
 
   render() {
-
+    console.log('props playlist', this.props.playlist)
     return (
       <div>
         <Player />
         <br />
-        <SearchForm  handleSubmit={this.handleSubmit}/>
+        <SearchForm handleSubmit={this.handleSubmit} />
         <br />
         <br />
+
         <audio
-          src={this.props.playlist.currentSong.audioUrl}
-          onClick={this.playSong}
+          autoPlay={true}
+          onEnded={this.nextTrack}
+          src={this.state.selectedSong}
+          onClick={this.handleSubmit}
           controls
           id="audio"
         />
+
         <div>
           {this.props.playlist.songList.map(index => {
-            return <div key={index.id}>{index.name}</div>;
+            return <div key={index.id}>{index.name}</div>
           })}
         </div>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
   playlist: state.playlist,
   form: state.form
-});
+})
 
 const mapDispatchToProps = dispatch => ({
   addSong: song => dispatch(addSongThunk(song))
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
+export default connect(mapStateToProps, mapDispatchToProps)(Playlist)
