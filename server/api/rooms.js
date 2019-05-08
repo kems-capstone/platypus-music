5c74138c944eabf80d3a283140443dc48fd3513d
Samiul Huque <Samiul.Huque@gmail.com>
2019-05-08 12:12:21 -0400

    added routes to query room table

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const {Room} = require('../db/models');
 
-router.get('/', (req, res, next) => {
-  res.send('This is the api for rooms');
+router.get('/', async (req, res, next) => {
+  const rooms = await Room.findAll({});
+  res.json(rooms);
+});
+
+router.post('/', async (req, res, next) => {
+  const roomName = req.body.name;
+  const createdRoom = await Room.create({name: roomName, closed: false});
+  res.json(createdRoom);
 });
 
 module.exports = router;
