6518c2a5c155d995886444dc633c4532972befff
Samiul Huque <Samiul.Huque@gmail.com>
2019-05-08 12:32:26 -0400

    backend route to create room now works. Route inly requires a name from the user and generates the random key on the backend

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -7,8 +7,23 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
+  function generateCode() {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let roomCode = '';
+    for (let i = 0; i < 4; i++) {
+      let j =
+        Math.floor(Math.random() * (Math.floor(35) - Math.ceil(0) + 1)) + 0;
+      roomCode += characters[j];
+    }
+    return roomCode;
+  }
   const roomName = req.body.name;
-  const createdRoom = await Room.create({name: roomName, closed: false});
+  const roomKey = generateCode();
+  const createdRoom = await Room.create({
+    name: roomName,
+    roomKey: roomKey,
+    closed: false
+  });
   res.json(createdRoom);
 });
 
