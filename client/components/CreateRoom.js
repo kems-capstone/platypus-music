758880d9dcf1488f7ad30fceaf126cd078a18f64
Samiul Huque <Samiul.Huque@gmail.com>
2019-05-08 11:42:01 -0400

    created room store and fixed navbar to properly link to dashboard

--- client/components/CreateRoom.js
+++ client/components/CreateRoom.js
@@ -1,42 +1,52 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {Field, reduxForm} from 'redux-form'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Field, reduxForm} from 'redux-form';
+import {Message} from 'semantic-ui-react';
 
 class CreateRoom extends Component {
   constructor() {
-    super()
-    this.handleSubmit = this.handleSubmit.bind(this)
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit() {
-    event.preventDefault()
-    console.log(this.props.form)
+    event.preventDefault();
+    console.log(this.props.form);
   }
   render() {
     return (
-      <div>
+      <div className="form-container">
         <h1>Create Room</h1>
-        <form name="createRoomForm" onSubmit={this.handleSubmit}>
+        <form
+          className="form-popup"
+          name="createRoomForm"
+          onSubmit={this.handleSubmit}
+        >
           <Field
+            className="form-input"
             type="text"
             component="input"
             name="room-name"
             placeholder="room name"
           />
+          <h3>Room Code:</h3>
+          <Message>1234</Message>
           <button type="submit">Create Room</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-const CreateRoomForm = reduxForm({
-  form: 'creat-room'
-})(CreateRoom)
+// const CreateRoomForm = reduxForm({
+//   form: 'creat-room'
+// })(CreateRoom);
 
 const mapStateToProps = state => {
   return {
     form: state.form
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, null)(CreateRoomForm)
+export default reduxForm({
+  form: 'creat-room'
+})(connect(mapStateToProps, null)(CreateRoom));
