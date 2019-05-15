35f913ebcd6714e1a46fb11c952b1bb7a015c925
Eileen Galindo <galindo.eileen@gmail.com>
2019-05-15 12:12:26 -0400

    working on styling

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Header, Icon, Image, Container} from 'semantic-ui-react';
+import {Header, Card, Container} from 'semantic-ui-react';
+// import patty from '../../public/patty.jpeg';
 
 class Homepage extends Component {
   render() {
@@ -9,17 +10,20 @@ class Homepage extends Component {
         <div className="homepage" id="homepageBackgrouund">
           <div className="homepageText">
             <Header as="h1" icon textAlign="center">
-              <Icon name="users" circular />
-              <Header.Content id="homePage-h1">Platypus Music</Header.Content>
+              <img id="patty" src="/patty.jpeg" />
+              <Card.Group itemsPerRow={2}>
+                <Card
+                  description="Platypus is a crowd-sourcing playlist app, that lets everyone be
+              the DJ."
+                  color="violet"
+                />
+                <Card
+                  description="Create your own room or enter the secret room code to join one
+              that is already playing."
+                  color="violet"
+                />
+              </Card.Group>
             </Header>
-            <p>
-              Platypus is a crowd-sourcing playlist app, that lets everyone be
-              the DJ.
-            </p>
-            <p>
-              Create your own room or enter the secret room code to join one
-              that is already playing.
-            </p>
           </div>
         </div>
       </Container>
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,52 +4,87 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
 
-import {Menu, Icon, Dropdown, Image, Button} from 'semantic-ui-react';
+import {
+  Menu,
+  Icon,
+  Dropdown,
+  Image,
+  Button,
+  Segment,
+  Input
+} from 'semantic-ui-react';
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <Menu color="violet" inverted id="navBar" fixed="top">
-    <Link className="nav-text" to="/home">
-      <Menu.Item id="navbar-title-and-image-container">
-        <div id="nav-title">
-          <div className="nav-title-text">Platypus</div>
-        </div>
-      </Menu.Item>
-    </Link>
-    {isLoggedIn ? (
+const Navbar = props => (
+  <div>
+    <Menu pointing>
+      <Menu.Item as="a" href="/home" name="home" />
       <Menu.Menu position="right">
-        <Menu.Item>
-          <Link className="nav-text" to="/dashboard">
-            Dashboard
-          </Link>
-        </Menu.Item>
+        <Menu.Item as="a" href="/dashboard" name="dashboard" />
         <Menu.Item id="menu-dropdown">
-          {/* <Button class="ui icon button"> */}
-          <Dropdown icon="user" button className="icon">
-            <Dropdown.Menu>
-              <Dropdown.Header
-                className="user"
-                content={<Link to="/userProfile">User Profile</Link>}
-              />
-              <Dropdown.Header
-                onClick={handleClick}
-                content={<Link to="/userProfile">Log Out</Link>}
-              />
-            </Dropdown.Menu>
-          </Dropdown>
-          {/* </Button> */}
+          <Button className="ui icon button">
+            <Dropdown icon="user" button className="icon">
+              <Dropdown.Menu>
+                <Dropdown.Header
+                  className="user"
+                  content={<Link to="/userProfile">User Profile</Link>}
+                />
+                <Dropdown.Header
+                  onClick={props.handleClick}
+                  content={<Link to="/userProfile">Log Out</Link>}
+                />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Button>
         </Menu.Item>
       </Menu.Menu>
-    ) : (
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Link to="/login">Login</Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/signup">Sign Up</Link>
-        </Menu.Item>
-      </Menu.Menu>
-    )}
-  </Menu>
+    </Menu>
+  </div>
+
+  // <Menu color="violet" inverted id="navBar" fixed="top">
+
+  // <Link className="nav-text" to="/home">
+  //   <Menu.Item id="navbar-title-and-image-container">
+  //     <div id="nav-title">
+  //       <div className="nav-title-text">Platypus</div>
+  //     </div>
+  //   </Menu.Item>
+  // </Link>
+
+  //   {isLoggedIn ? (
+  // <Menu.Menu position="right">
+  //   <Menu.Item>
+  //     <Link className="nav-text" to="/dashboard">
+  //       Dashboard
+  //     </Link>
+  //   </Menu.Item>
+  // <Menu.Item id="menu-dropdown">
+  //   <Button class="ui icon button">
+  //   <Dropdown icon="user" button className="icon">
+  //     <Dropdown.Menu>
+  //       <Dropdown.Header
+  //         className="user"
+  //         content={<Link to="/userProfile">User Profile</Link>}
+  //       />
+  // <Dropdown.Header
+  //   onClick={handleClick}
+  //   content={<Link to="/userProfile">Log Out</Link>}
+  // />
+  //     </Dropdown.Menu>
+  //   </Dropdown>
+  //   </Button>
+  // </Menu.Item>
+  //     </Menu.Menu>
+  //   ) : (
+  //     <Menu.Menu position="right">
+  //       <Menu.Item>
+  //         <Link to="/login">Login</Link>
+  //       </Menu.Item>
+  //       <Menu.Item>
+  //         <Link to="/signup">Sign Up</Link>
+  //       </Menu.Item>
+  //     </Menu.Menu>
+  //   )}
+  // </Menu>
 );
 
 /**
