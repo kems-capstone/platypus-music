af13d32849ddd9707f3a7904038371a83d777bf7
Eileen Galindo <galindo.eileen@gmail.com>
2019-05-16 19:22:25 -0400

    fixed votes

--- client/store/roomReducer.js
+++ client/store/roomReducer.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import io from 'socket.io-client';
-import listenForUpdatePlaylistThunk from './playlist';
+import {voteThunk} from './playlist';
 import history from '../history';
 
-import socket from '../socket'
+import socket from '../socket';
+import {Playlist} from '../components';
 
 const ADD_ROOM = 'ADD_ROOM';
 const GET_ROOM = 'GET_ROOM';
@@ -50,7 +51,7 @@ const closeRoom = roomId => {
 const refreshRoomState = roomState => {
   return {
     type: REFRESH_ROOM,
-   roomState
+    roomState
   };
 };
 
@@ -71,15 +72,15 @@ export const addRoomThunk = (roomName, user) => {
 };
 
 export const joinRoomThunk = key => async dispatch => {
-  console.log('In Join Room Thunk')
+  console.log('In Join Room Thunk');
   const room = await axios.get('/api/rooms/join/' + key);
-  console.log('room in join room thunk', room)
+  console.log('room in join room thunk', room);
   const roomInfo = {
     room: room.data.room,
     members: room.data.members,
     host: false
   };
-  console.log('roomInfo in join room thunk', roomInfo)
+  console.log('roomInfo in join room thunk', roomInfo);
 
   if (roomInfo.room) {
     dispatch(joinRoom(roomInfo));
@@ -109,14 +110,54 @@ export const closeRoomThunk = roomId => async dispatch => {
   await axios.put(`/api/rooms/${roomId}`);
 
   dispatch(closeRoom(roomId));
-  socket.emit('hostEnded')
+  socket.emit('hostEnded');
   history.push('/dashboard');
 };
-
+const refreshVotes = async (roomId, song, mrId) => {
+  let {data} = await axios.post(`/api/rooms/${roomId}/vote/${song}`, {
+    mrId: mrId
+  });
+  return data;
+};
 export const refreshRoom = () => async dispatch => {
   const roomData = await axios.get('/api/rooms/refresh');
+  let arr = [];
+  for (let i = 0; i < roomData.data.songArray.length; i++) {
+    let song = roomData.data.songArray[i];
+    let voteCount = await refreshVotes(song.roomId, song.musicId, song.id);
+    console.log('VOTE COUNTTT', voteCount);
+    song.voteCount = voteCount;
+    arr.push(song);
+  }
+  console.log('ARRRR', arr); //votes
+  // let songsWithVotes = roomData.data.songArray.map(song => {
+  //   let voteCount = refreshVotes(song.roomId, song.musicId, song.id);
+  //   console.log('VOTE COUNTTT', voteCount);
+  //   song.voteCount = voteCount;
+  //   return song;
+  // });
+  // console.log('SONGS WITH VOTES', songsWithVotes);
+  let songInfo = roomData.data.room.music;
 
-  socket.emit('getRoomGotPlaylist', roomData.data.room.music);
+  console.log('roomData.data.room.music', roomData.data.room.music);
+
+  let newPlaylist = [];
+  for (let i = 0; i < songInfo.length; i++) {
+    let song = songInfo[i];
+    for (let j = 0; j < arr.length; j++) {
+      if (song.id === arr[j].musicId) {
+        song.voteCount = arr[j].voteCount;
+        newPlaylist.push(song);
+      }
+    }
+  }
+
+  newPlaylist.sort((a, b) => {
+    return a.voteCount > b.voteCount ? -1 : b.voteCount > a.voteCount ? 1 : 0;
+  });
+
+  roomData.data.room.music = newPlaylist;
+  socket.emit('getRoomGotPlaylist', newPlaylist);
 
   dispatch(refreshRoomState(roomData.data));
 };
@@ -132,7 +173,7 @@ export default function(state = initialState, action) {
     case GET_ROOM:
       return action.roomData;
     case REFRESH_ROOM:
-      return action.roomState
+      return action.roomState;
     case ADD_ROOM:
       return action.roomObject;
     case JOIN_ROOM:
